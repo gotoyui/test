602b465307304a2a0e39d6f3ff0f6206205ae112
gotoyui <s3011065@surugadai.ac.jp>
2015-01-22 17:00:58 +0900

    test アップロード

---
+++ src/com/example/test/Test.java
@@ -0,0 +1,32 @@
+package com.example.test;
+
+import java.io.*;
+
+public class Test {
+
+	// eラーニングのサンプル問題解答用クラス
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		try {
+			while (true) {
+				System.out.println("西暦を入力してください(0を入力したら終了します。)");
+				int year = Integer.parseInt(reader.readLine());
+				if (year == 0) {
+					System.out.println("処理を終了します。");
+					break;
+					// 4で割れて、100で割れる。もしくは400で割れる　→　うるう年！
+				} else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+					System.out.println(year + "年はうるう年です。");
+					// その他 → うるう年ではない！
+				} else {
+					System.out.println(year + "年はうるう年ではありません。");
+				}
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		} catch (NumberFormatException e) {
+			System.out.println("数字を入力してください。");
+		}
+	}
+}
